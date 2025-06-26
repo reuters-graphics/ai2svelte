2acfb9e8871d78cd153c9d4b6a83dd4b96a3ffdc
Sudev Kiyada <sudev.kiyada@thomsonreuters.com>
2025-06-26 11:11:14 +0530

    init

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'My Docs',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+		}),
+	],
+});
