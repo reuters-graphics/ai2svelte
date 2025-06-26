09e7d2cd36a6f5d3ad12606c6d7b4000ae0f998f
Sudev Kiyada <sudev.kiyada@thomsonreuters.com>
2025-06-26 11:29:16 +0530

    new hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,25 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'ai2svelte',
+			logo: {
+				light: './src/assets/logo-light.svg',
+				dark: './src/assets/logo-dark.svg',
+				replacesTitle: true,
+			},
+			favicon:
+				'https://graphics.thomsonreuters.com/style-assets/images/logos/favicon/favicon.ico',
+			head: [
+				{
+				tag: 'link',
+				attrs: {
+					rel: 'icon',
+					href: 'https://graphics.thomsonreuters.com/style-assets/images/logos/favicon/favicon.ico',
+					sizes: '32x32',
+				},
+				},
+			],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/reuters-graphics/ai2svelte/' }],
 		}),
 	],
 });
