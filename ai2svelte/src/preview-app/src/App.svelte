c1854987237dbba421688469d708b250bd9279d5
Sudev Kiyada <sudev.kiyada@thomsonreuters.com>
2026-01-05 17:09:21 +0530

    setup preview app for hosting preview

---
+++ ai2svelte/src/preview-app/src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Framer from "./lib/Framer.svelte";
+
+  // STYLE IMPORT
+  import "./app.scss";
+</script>
+
+<main>
+  <Framer />
+</main>
+
+<style>
+  main {
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
